86643682fd9d8ed75c9d88f1ee0e1dad85cff6eb
Harshini-Medha <109570543+Harshini-Medha@users.noreply.github.com>
2026-06-17 12:31:30 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,5 +1,52 @@
 package com.example;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        try {
+
+            WebDriverManager.chromedriver().setup();
+
+            ChromeOptions options = new ChromeOptions();
+
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--disable-gpu");
+            options.addArguments("--remote-allow-origins=*");
+
+            WebDriver driver = new ChromeDriver(options);
+
+            driver.get("https://www.saucedemo.com/");
+
+            driver.findElement(By.id("user-name"))
+                    .sendKeys("standard_user");
+
+            driver.findElement(By.id("password"))
+                    .sendKeys("secret_sauce");
+
+            driver.findElement(By.id("login-button"))
+                    .click();
+
+            System.out.println("TITLE AFTER LOGIN: "
+                    + driver.getTitle());
+
+            driver.quit();
+
+        } catch (Exception e) {
+            System.out.println("ERROR: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}package com.example;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
