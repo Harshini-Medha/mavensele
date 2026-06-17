6af06ce3411b1534880ebcfec6363965323ee2fe
Harshini-Medha <109570543+Harshini-Medha@users.noreply.github.com>
2026-06-17 12:32:59 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -45,48 +45,4 @@ public class App {
             e.printStackTrace();
         }
     }
-}package com.example;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
-
-public class App {
-
-    public static void main(String[] args) {
-
-        // Setup driver FIRST
-        WebDriverManager.chromedriver().setup();
-
-        // Chrome options
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless=new");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--disable-gpu");
-        options.addArguments("--remote-allow-origins=*");
-
-        // Create driver with options
-        WebDriver driver = new ChromeDriver(options);
-
-        try {
-            driver.get("https://www.saucedemo.com/");
-            driver.manage().window().maximize();
-
-            driver.findElement(By.id("user-name"))
-                  .sendKeys("standard_user");
-
-            driver.findElement(By.id("password"))
-                  .sendKeys("secret_sauce");
-
-            driver.findElement(By.id("login-button"))
-                  .click();
-
-        } finally {
-            driver.quit();
-        }
-    }
 }
