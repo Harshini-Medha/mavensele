f545c8f414b2d35f32d7be68dc6a23c0825798ac
Harshini-Medha <109570543+Harshini-Medha@users.noreply.github.com>
2026-06-17 12:26:39 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,31 +3,43 @@ package com.example;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
-/**
- * Hello world!
- *
- */
-public class App
-{
-    public static void main(String[] args)
-    {
-        WebDriver driver = new ChromeDriver();
-        ChromeOptions options = new ChromeOptions(); options.addArguments("--headless=new"); options.addArguments("--no-sandbox"); options.addArguments("--disable-dev-shm-usage"); options.addArguments("--disable-gpu"); options.addArguments("--remote-allow-origins=*");
-        WebDriverManager.chromedriver().setup(); 
-        driver.get("https://www.saucedemo.com/");
-        driver.manage().window().maximize();
+import io.github.bonigarcia.wdm.WebDriverManager;
 
-        driver.findElement(By.id("user-name"))
-              .sendKeys("standard_user");
+public class App {
 
-        driver.findElement(By.id("password"))
-              .sendKeys("secret_sauce");
+    public static void main(String[] args) {
 
-        driver.findElement(By.id("login-button"))
-              .click();
+        // Setup driver FIRST
+        WebDriverManager.chromedriver().setup();
+
+        // Chrome options
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--remote-allow-origins=*");
+
+        // Create driver with options
+        WebDriver driver = new ChromeDriver(options);
+
+        try {
+            driver.get("https://www.saucedemo.com/");
+            driver.manage().window().maximize();
+
+            driver.findElement(By.id("user-name"))
+                  .sendKeys("standard_user");
+
+            driver.findElement(By.id("password"))
+                  .sendKeys("secret_sauce");
+
+            driver.findElement(By.id("login-button"))
+                  .click();
+
+        } finally {
+            driver.quit();
+        }
     }
 }
-
-
-
